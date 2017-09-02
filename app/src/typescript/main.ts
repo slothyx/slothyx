9ebc679bbdbfbd4ede56b5b1886fa3c3066ec91c
Patrick Obermueller <pobermueller.mmt-b2015@fh-salzburg.ac.at>
2017-09-02 12:32:51 +0200

    apply editorconfig to *.html, *.ts

--- app/src/typescript/main.ts
+++ app/src/typescript/main.ts
@@ -3,6 +3,7 @@ import {greetPerson} from "./utility/greeter";
 function showHello(divName: string, name: string) {
     const el = document.getElementById(divName);
     el.innerText = greetPerson(name);
+
     console.log("hi2");
 }
 
