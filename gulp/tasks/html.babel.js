08f48aeb6e878f7d00744d136b1e51ecd68d49d9
Patrick Obermueller <pobermueller.mmt-b2015@fh-salzburg.ac.at>
2017-09-02 12:26:00 +0200

    add html minifier

--- gulp/tasks/html.babel.js
+++ gulp/tasks/html.babel.js
@@ -1,10 +1,12 @@
 import gulp from "gulp";
 import CONFIG from "../config.babel";
+import htmlmin from "gulp-htmlmin";
 
 gulp.task("html", function () {
     const src = `${CONFIG.src}/html/**/*.html`;
     const dist = `${CONFIG.dist}/html/`;
 
     return gulp.src(src)
+        .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest(dist));
 });
